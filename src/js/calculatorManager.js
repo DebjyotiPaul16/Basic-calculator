7c4b4f98e8b48c72f5ba718c3feb8bf5d8d72549
debayan.das <debayan.das@learningmatekol.local>
2017-06-23 01:08:13 +0530

    templating added for calculator

--- src/js/calculatorManager.js
+++ src/js/calculatorManager.js
@@ -1,7 +1,5 @@
 import Calculator from "./calculator.js";
 import {
-    closeButton,
-    minimizeButton,
     calculator_data,
     operator,
     memory_operations,
@@ -9,130 +7,78 @@ import {
 } from './calculatorConfig.js';
 export default class CalculatorManger {
     createCalculator() {
-        var calculatordiv, mainDiv, div, table, toolbar, toolbarTd, tr0, td01;
-        calculatordiv = document.createElement('div');
-        calculatordiv.setAttribute('id', 'calculator');
-        mainDiv = document.createElement('div');
-        mainDiv.setAttribute('id', 'drag');
-        div = this.createDiv();
-        table = this.createTable();
-        toolbar = document.createElement('tr'); //create the toolbar
-        toolbarTd = this.createToolbar();
-        toolbar.appendChild(toolbarTd);
-        tr0 = document.createElement('Tr'); //calculator screen to display output
-        td01 = document.createElement('Td');
-        td01.setAttribute('colspan', 5);
-        td01.innerHTML = '<span id="disp_sign" style="height:40px; display:block"></span><span class="disp_btn" id="disp" name="display" type="text" style="line-height:40px; display:block"></span>';
-        tr0.appendChild(td01);
-        table.appendChild(toolbar);
-        table.appendChild(tr0);
+        let calculatordiv =
+            `<div id="calculator">
+                <div id="drag">
+                    <div id="minimizeCalc" style="cursor: pointer;display: inline-block">
+                        <button id="calc_icon"></button>
+                    </div>
+                    <table id="calc" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td style="text-align: right;background-color: #1A2533" colspan="5">
+                                <button class="minimize" aria-label="minimize">&minus;</button>
+                                <button class="close-calculator" aria-label="close">x</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="5">
+                                <span id="disp_sign" style="height:40px; display:block"></span>
+                                <span class="disp_btn" id="disp" name="display" type="text" style="line-height:40px; display:block"></span>
+                            </td>
+                        </tr>
+                        ${calculator_data.map((rowData) =>{
+                        return `<tr>
+                          ${rowData.map((columnData) =>{
+                                return `<td>${this.createCalculatorButton(columnData)}</td>`;
+                            }).join("")}
+                        </tr>`;
+                        }).join("")}    
+                    </table>
+                </div>
+            </div>`;
 
-        for (var row = 0; row < calculator_data.length; row++) {
-            var tr = document.createElement('Tr');
-            for (var col = 0; col < calculator_data[row].length; col++) {
-                var td = this.createCalculatorButton(row, col);
-                tr.appendChild(td);
-            }
-            table.appendChild(tr);
-        }
-
-        mainDiv.appendChild(div);
-        mainDiv.appendChild(table);
-        calculatordiv.appendChild(mainDiv);
-        if ($("#simple-calculator").length) {
-            this.attachCalculatorTool(div, table, calculatordiv);
-        } else {
-            this.attachCalculatorBody(div, table, calculatordiv);
-        }
-        var display = document.getElementById('disp');
+        let calcElem = this.attachCalculatorBody(calculatordiv);
+        let display = calcElem.find("#disp").get(0);
         this.calcobj = new Calculator(display);
         this.operateCalculator(this.calcobj);
-        this.draggable();
+        this.makeDraggable(calcElem);
         this.handleWithKeyboard(this.calcobj);
-        this.calculatorShowHide(this);
+        this.calculatorShowHide();
     }
 
-    createDiv() {
-        var div = document.createElement('div');
-        div.innerHTML = '<button id="calc_icon"></button>';
-        div.setAttribute('id', 'minimizeCalc');
-        div.style.cursor = 'pointer';
-        div.style.display = 'inline-block';
-        return div;
+    attachCalculatorBody(calculatorStr) {
+        let calculatorElm = $(calculatorStr);
+        calculatorElm.find('table').css('display', 'block');
+        $('body').prepend(calculatorElm);
+        return calculatorElm;
     }
 
-    createTable() {
-        var table = document.createElement("Table");
-        table.setAttribute('id', 'calc');
-        table.setAttribute('cellpadding', 0);
-        table.setAttribute('cellspacing', 0);
-        table.setAttribute('border', 2);
-        table.style.display = 'block';
-        return table;
-    }
+    createCalculatorButton(columnData) {
+        var elemValue = columnData.value;
+        var label = (columnData.label) ? 'aria-label="' + columnData.label + '"' : '';
 
-    createToolbar() {
-        var toolbarTd = document.createElement('td');
-        toolbarTd.innerHTML = '<button class="minimize" aria-label="minimize">&minus;</button><button class="close-calculator" aria-label="close">x</button>';
-        toolbarTd.style.textAlign = 'right';
-        toolbarTd.style.backgroundColor = '#1A2533';
-        toolbarTd.setAttribute('colspan', '5');
-        return toolbarTd;
-    }
-
-    attachCalculatorTool(div, table, calculatordiv) {
-        document.getElementById("simple-calculator").innerHTML = '<button id="calc-icon-tool" class="maximize" aria-label="calculator tool"></button>';
-        $(div).find('img').css('display', 'none');
-        $(table).css('display', 'none');
-        $("#simple-calculator").prepend(calculatordiv);
-        if (!closeButton) {
-            $(".close-calculator").css("display", "none");
-        }
-        if (!minimizeButton) {
-            $(".minimize").css("display", "none");
-        }
-    }
-
-    attachCalculatorBody(div, table, calculatordiv) {
-        $(div).find('img').css('display', 'none');
-        $(table).css('display', 'block');
-        $('body').prepend(calculatordiv);
-        if (!closeButton) {
-            $(".close-calculator").css("display", "none");
-        }
-        if (!minimizeButton) {
-            $(".minimize").css("display", "none");
-        }
-    }
-
-    createCalculatorButton(row, col) {
-        var td = document.createElement('td');
-        var elemValue = calculator_data[row][col].value;
-        var label = (calculator_data[row][col].label) ? 'aria-label="' + calculator_data[row][col].label + '"' : '';
         if (!isNaN(elemValue) || elemValue === ".") {
-            td.innerHTML = '<button class="btn opeationButton" ' + label + ' operation="setValue"  value="' + elemValue + '">' + calculator_data[row][col].name + '</button><span class="sr-only">&nbsp;</span>';
-        } else {
-            if (operator.indexOf(elemValue) !== -1) {
-                if (elemValue === '%') {
-                    td.innerHTML = '<button role="button" class="btn opeationButton" ' + label + '  operation="calculatePercentage"  value="' + elemValue + '">' + calculator_data[row][col].name + '</button>';
-                } else if (elemValue === '=') {
-                    td.innerHTML = '<button role="button"  class="btn opeationButton" ' + label + '  operation="getResult" value="' + elemValue + '" >' + calculator_data[row][col].name + '</button>';
-                } else {
-                    td.innerHTML = '<button  role="button" class="btn opeationButton" ' + label + '  operation="setSign" value="' + elemValue + '">' + calculator_data[row][col].name + '</button>';
-                }
-
-            } else if (memory_operations.indexOf(elemValue) !== -1) {
-                td.innerHTML = '<button role="button" class="btn opeationButton" ' + label + '  operation="memoryOperations" value="' + elemValue + '">' + calculator_data[row][col].name + '</button>';
-            } else if (clear_operation.indexOf(elemValue) !== -1) {
-                td.innerHTML = '<button role="button" class="btn opeationButton" ' + label + '  operation="clearData" value="' + elemValue + '">' + calculator_data[row][col].name + '</button>';
-            }
-
+            return '<button class="btn opeationButton" ' + label + ' operation="setValue"  value="' + elemValue + '">' + columnData.name + '</button><span class="sr-only">&nbsp;</span>';
         }
-        return td;
+        if (operator.indexOf(elemValue) !== -1) {
+            if (elemValue === '%') {
+                return '<button role="button" class="btn opeationButton" ' + label + '  operation="calculatePercentage"  value="' + elemValue + '">' + columnData.name + '</button>';
+            }
+            if (elemValue === '=') {
+                return '<button role="button"  class="btn opeationButton" ' + label + '  operation="getResult" value="' + elemValue + '" >' + columnData.name + '</button>';
+            }
+            return '<button  role="button" class="btn opeationButton" ' + label + '  operation="setSign" value="' + elemValue + '">' + columnData.name + '</button>';
+        }
+        if (memory_operations.indexOf(elemValue) !== -1) {
+            return '<button role="button" class="btn opeationButton" ' + label + '  operation="memoryOperations" value="' + elemValue + '">' + columnData.name + '</button>';
+        }
+        if (clear_operation.indexOf(elemValue) !== -1) {
+            return '<button role="button" class="btn opeationButton" ' + label + '  operation="clearData" value="' + elemValue + '">' + columnData.name + '</button>';
+        }
     }
 
     operateCalculator(calcobj) {
-        $(document).off().on('click', '.opeationButton', function () {
+        $(document).off('click', '.opeationButton').on('click', '.opeationButton', function () {
             var operation = $(this).attr('operation');
             if (operation === "setValue") {
                 calcobj.setValue($(this).val());
@@ -150,20 +96,20 @@ export default class CalculatorManger {
         })
     }
 
-    draggable() {
-        $("#calculator").css({ zIndex: 999999 });
-        $("#drag").draggable({
+    makeDraggable(calcElem) {
+        calcElem.find("#calculator").css({zIndex: 999999});
+        calcElem.find("#drag").draggable({
             containment: 'body',
             scroll: true,
             scrollSpeed: 100,
             cursor: 'move',
             cancel: false,
             start: function (event, ui) {
-                $('#calc_icon').removeClass('maximize');
+                calcElem.find('#calc_icon').removeClass('maximize');
             },
             stop: function (event, ui) {
                 setTimeout(function () {
-                    $('#calc_icon').addClass('maximize');
+                    calcElem.find('#calc_icon').addClass('maximize');
                 }, 200);
             }
         });
@@ -172,7 +118,7 @@ export default class CalculatorManger {
 
     handleWithKeyboard(calcobj) {
         $(document).off('keyup').on('keyup', function (event) {
-            var operator = { 107: '+', 109: '-', 106: '*', 111: '/' };
+            var operator = {107: '+', 109: '-', 106: '*', 111: '/'};
             if (!isNaN(event.key) && event.keyCode !== 32) {
                 calcobj.setValue(event.key);
             } else if (event.keyCode === 110) {
@@ -187,19 +133,19 @@ export default class CalculatorManger {
         });
     }
 
-    calculatorShowHide(th) {
+    calculatorShowHide() {
         var self = this;
-        $(document).on('click', '.minimize', function () {
+        $(document).off('click', '.minimize').on('click', '.minimize', function () {
             $('#calc_icon').addClass('maximize').attr('aria-label', 'Maximize calculator');
-            th.minimize();
+            self.minimize();
             $(document).off("keyup");
         });
-        $(document).on('click', '.maximize', function () {
+        $(document).off('click', '.maximize').on('click', '.maximize', function () {
             self.handleWithKeyboard(self.calcobj);
-            th.maximize();
+            self.maximize();
         });
-        $(document).on('click', '.close-calculator', function () {
-            th.closeCalculator();
+        $(document).off('click', '.close-calculator').on('click', '.close-calculator', function () {
+            self.closeCalculator();
             $(document).off("keyup");
         });
     }
@@ -238,7 +184,6 @@ export default class CalculatorManger {
 
     closeCalculator() {
         var display = document.getElementById('disp');
-        // var calcobj = new Calculator(display);
         this.calcobj.clearData('c');
         document.getElementById("calc").style.display = "none";
         document.getElementById("drag").style.top = 0;
