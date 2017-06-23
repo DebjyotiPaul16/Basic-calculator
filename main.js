aaf4d067fbe04a5b372022d1375a624a03652f62
ArnabBanerji <arnab.banerji@pearson.com>
2017-06-23 16:41:47 +0530

    Merge branches 'master' and 'templateChange' of https://github.com/DebjyotiPaul16/Basic-calculator
    # Conflicts:
    #	src/js/calculatorManager.js

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var btn = document.getElementById("close-calc");
 var btnShow = document.getElementById("show-calc");
 var clearCalc = document.getElementById("clear-calc");
+var spawnCalc = document.getElementById("spawn-calc");
+
 
 var BasicCalculator = new calculator.loadCalculator();
 // BasicCalculator.loadDependencyAndCreate();
-//BasicCalculator.hideCalculator();
-
 btn.addEventListener("click", BasicCalculator.hideCalculator.bind(BasicCalculator), false);
 btnShow.addEventListener("click", BasicCalculator.showCalculator.bind(BasicCalculator), false);
 clearCalc.addEventListener("click", BasicCalculator.clearCalculator.bind(BasicCalculator), false);
+spawnCalc.addEventListener("click", function () {
+    BasicCalculator.validateLocation(200, 500);
+    BasicCalculator.moveCalculator();
+});
