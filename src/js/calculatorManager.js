662fca208ceb1d0560828b58331b7432a2d0bac0
djPaul93 <debjyoti.paul1993@gmail.com>
2017-07-01 00:31:19 +0530

    arrow issue fixed

--- src/js/calculatorManager.js
+++ src/js/calculatorManager.js
@@ -22,9 +22,12 @@ export default class CalculatorManger {
                         </tr>
                         <tr>
                             <td colspan="4">
-                                <div class="calcDiv"><span class="disp_btn" id="disp_eqn" name="display" type="text" style="line-height:40px; display:block"></span></div>
-																<div class="seek seekLeft"></div>
-																<div class="seek seekRight"></div>
+                                <div class="calcDiv">
+                                <span class="disp_btn" id="disp_eqn" name="display" type="text" style="line-height:40px; display:block">
+								</span>
+								 <div class="seek seekLeft"></div>
+								 <div class="seek seekRight"></div>
+								</div>									
                             </td>
                         </tr>
                         <tr>
