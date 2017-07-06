a07854fb2c59fc79e3634848832faf0da8d09e9a
debayan.das <debayan.das@learningmate.com>
2017-07-06 18:06:43 +0530

    Test cases updated

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -111,7 +111,7 @@ export default class Calculator {
     }
 
     _checkOverflow() {
-        if (this._displayEqnDiv.innerText.length * 7.5 > this._displayResultDiv.offsetWidth) {
+        if (this._displayEqnDiv.innerHTML.length * 7.5 > this._displayResultDiv.offsetWidth) {
             this._displayEqnDiv.parentElement.querySelector(".seekLeft").style.display = 'inline-block';
             this._displayEqnDiv.parentElement.querySelector(".seekLeft").setAttribute("aria-label","Left");
         }
--- src/test/calculator.spec.js
+++ src/test/calculator.spec.js
@@ -4,10 +4,14 @@ fdescribe("test suite for calculator.js", ()=> {
     let calcObj, displayResultDiv, displayEqnDiv;
     beforeEach(()=> {
         displayResultDiv = {
-            innerHTML: "result"
+            innerHTML: "result",
+            setAttribute: jasmine.createSpy("setAttribute"),
+            focus: jasmine.createSpy("focus")
         };
         displayEqnDiv = {
-            innerHTML: "eqn"
+            innerHTML: "eqn",
+            setAttribute: jasmine.createSpy("setAttribute"),
+            focus: jasmine.createSpy("focus")
         };
         calcObj = new Calculator(displayResultDiv, displayEqnDiv);
     });
@@ -64,242 +68,122 @@ fdescribe("test suite for calculator.js", ()=> {
 
     describe("_evalResult method", ()=> {
         it("should prevent dividing by zero", ()=> {
-            this._eqnArr = ["7", "/", "0"];
-            expect(calcObj._displayResultDiv.innerHTML).toBe("5");
+            calcObj._eqnArr = ["7", "/", "0"];
+            calcObj._evalResult();
+            expect(calcObj._displayResultDiv.innerHTML).toBe('Cannot divide by zero');
+        });
+        it("should display result properly when no decimal is included", ()=> {
+            calcObj._eqnArr = ["1", "+", "3"];
+            calcObj._evalResult();
+            expect(calcObj._displayResultDiv.innerHTML).toBe('4');
+        });
+        it("should display result properly when decimal is included", ()=> {
+            calcObj._eqnArr = ["1", "/", "3"];
+            calcObj._evalResult();
+            expect(calcObj._displayResultDiv.innerHTML).toBe('0.3333333334');
+        });
+        it("should display result properly when decimal is included", ()=> {
+            calcObj._eqnArr = ["14", "/", "3"];
+            calcObj._evalResult();
+            expect(calcObj._displayResultDiv.innerHTML).toBe('4.6666666667');
         });
     });
 
+    it("_renderEqn method", ()=> {
+        calcObj._eqnArr = ["14", "/", "3"];
+        calcObj._renderEqn();
+        expect(calcObj._displayEqnDiv.innerHTML).toBe('14 &divide 3');
+    });
 
-    describe("clearData test suite", ()=> {
-        it("should test it when cleartype = c", ()=> {
+    describe("setSign method", ()=> {
+        it("should not perform if result is undefined", ()=> {
+            calcObj._isResultUndefined = true;
+            expect(calcObj.setSign("+")).toBe(undefined);
+        });
+        it("should replace the operator if already inserted", ()=> {
+            calcObj._isOperatorInserted = true;
+            calcObj._eqnArr = ["+"];
+            calcObj.setSign("*");
+            expect(calcObj._eqnArr).toEqual(["*"])
+        });
+        it("should eval the result", ()=> {
+            calcObj._eqnArr = ["7", "+"];
+            calcObj._result = "9";
+            calcObj.setSign("*");
+            expect(calcObj._displayEqnDiv.innerHTML).toBe('7 + 9 &times');
+            expect(calcObj._displayResultDiv.innerHTML).toBe('16');
+        });
+    });
+
+    describe("clearData method", ()=> {
+        beforeEach(()=> {
+            spyOn(calcObj, "_resetArrows");
+        });
+        it("should clear both result and eqn if 'C' is pressed", ()=> {
+            calcObj._result = "result";
+            calcObj._eqnArr = ["eqn"];
             calcObj.clearData("c");
-            expect(calcObj.initialValue).toBe('');
-            expect(calcObj.lastValue).toBe('');
-            expect(calcObj.operator).toBe('');
-            expect(calcObj.displayOperator).toBe('');
-            expect(calcObj.dispResult).toBe(false);
-            expect(calcObj.display.innerHTML).toBe('');
+            expect(calcObj._displayEqnDiv.innerHTML).toBe('');
+            expect(calcObj._displayResultDiv.innerHTML).toBe('0');
         });
-        describe("should test for cleartype = 'bs' ", ()=> {
-            it("should test when lastValue is true", function () {
-                calcObj.lastValue = 26;
-                calcObj.display.innerHTML = "25";
-                calcObj.clearData("bs");
-                expect(calcObj.lastValue).toBe(2);
-                expect(calcObj.display.innerHTML).toBe("25");
-            });
-            it("should test when operator is true", function () {
-                calcObj.lastValue = false;
-                calcObj.operator = "+";
-                calcObj.clearData("bs");
-                expect(calcObj.operator).toBe('');
-                expect(calcObj.displayOperator).toBe('');
-            });
-            it("should test when initialvalue is true", function () {
-                calcObj.lastValue = false;
-                calcObj.operator = false;
-                calcObj.initialValue = 25;
-                calcObj.clearData("bs");
-                expect(calcObj.initialValue).toBe(2);
-            });
-            it("should test when all false", function () {
-                calcObj.display.innerHTML = "52";
-                calcObj.lastValue = false;
-                calcObj.operator = false;
-                calcObj.initialValue = false;
-                calcObj.clearData("bs");
-                expect(calcObj.display.innerHTML).toBe("5");
-            });
+
+        it("should return if backspace is pressed and result is undefined", ()=> {
+            calcObj._isResultUndefined = true;
+            expect(calcObj.clearData("bs")).toBe(undefined);
         });
-        describe("test suite for cleartype 'cd'", ()=> {
-            it("should test when lastvalue is true", function () {
-                calcObj.lastValue = "45";
-                calcObj.initialValue = "";
-                calcObj.displayOperator = "+";
-                calcObj.clearData('ce');
-                expect(calcObj.lastValue).toBe('');
-                expect(calcObj.display.innerHTML).toBe("+0");
-            });
-            it("should test when lastvalue is false", function () {
-                calcObj.lastValue = false;
-                calcObj.clearData("ce");
-                expect(calcObj.initialValue).toBe('');
-                expect(calcObj.operator).toBe('');
-                expect(calcObj.displayOperator).toBe('');
-                expect(calcObj.display.innerHTML).toBe(0);
-            });
-            it("nothing is true", function () {
-                spyOn(window.console, "info");
-                calcObj.clearData("abc");
-                expect(window.console.info).toHaveBeenCalled();
-            });
+
+        it("should delete the last of result", ()=> {
+            calcObj._result = "23";
+            calcObj.clearData('bs');
+            expect(calcObj._displayResultDiv.innerHTML).toBe('2');
+        });
+        it("should update result to zero", ()=> {
+            calcObj._result = "2";
+            calcObj.clearData('bs');
+            expect(calcObj._displayResultDiv.innerHTML).toBe('0');
+        });
+        it("should clear only result if 'CE' is pressed", ()=> {
+            calcObj._result = "result";
+            calcObj._eqnArr = ["eqn"];
+            calcObj.clearData("ce");
+            expect(calcObj._eqnArr).toEqual(["eqn"]);
+            expect(calcObj._displayResultDiv.innerHTML).toBe('0');
+        });
+
+        it("should return if any other than 'C','CE','bs' is pressed", ()=> {
+            expect(calcObj.clearData("else")).toBe(undefined);
         });
     });
 
-    describe("setValue test suite", function () {
-        describe("test suite for setValue when this.initialValue is false", function () {
-            it("should test when intialvalue is false", function () {
-                calcObj.initialValue = false;
-                calcObj.setValue("20.23");
-                expect(calcObj.initialValue).toBe('20.23');
-                expect(calcObj.display.innerHTML).toBe('20.23');
-            });
-            it("should test when intialvalue is false and this.initialValue.indexOf('.') === 0", function () {
-                calcObj.initialValue = false;
-                calcObj.setValue(".23");
-                expect(calcObj.initialValue).toBe('0.');
-            });
+    describe("getResult method", ()=> {
+        beforeEach(()=> {
+            spyOn(calcObj, "_resetArrows");
         });
-        describe("test suite for this.operator is false", function () {
-            it("should test when this.dispResult is true", function () {
-                calcObj.dispResult = true;
-                calcObj.initialValue = true;
-                calcObj.operator = false;
-                calcObj.setValue("20.23");
-                expect(calcObj.initialValue).toBe("20.23");
-                expect(calcObj.dispResult).toBe(false);
-            });
-            it("should test when !this.operator is false and this.dispResult false", function () {
-                calcObj.initialValue = "20";
-                calcObj.dispResult = false;
-                calcObj.operator = false;
-                calcObj.setValue(".");
-                expect(calcObj.initialValue).toBe("20.");
-            });
-            it("should test when this.initialValue.indexOf('.') === 0", function () {
-                calcObj.initialValue = ".";
-                calcObj.dispResult = true;
-                calcObj.operator = false;
-                calcObj.setValue(".");
-                expect(calcObj.initialValue).toBe("0.");
-            });
-            it("should test when val === '.' && this.initialValue.indexOf('.') >= 0", function () {
-                calcObj.initialValue = ".";
-                calcObj.dispResult = false;
-                calcObj.operator = false;
-                calcObj.setValue(".");
-                expect(calcObj.initialValue).toBe(".");
-            });
-            it("should test when this.initialValue.toString().length > 28", function () {
-                calcObj.initialValue = "abcdefghijklmnopqrstuvwxyzabcdefghijkl";
-                calcObj.dispResult = false;
-                calcObj.operator = false;
-                calcObj.setValue(".");
-                expect(calcObj.display.innerHTML).toBe("lmnopqrstuvwxyzabcdefghijkl.");
-            });
+        it("should return if result is undefined", ()=> {
+            calcObj._isResultUndefined = true;
+            expect(calcObj.getResult()).toEqual(undefined);
         });
-        describe("test suite for setValue when this.operator is true", function () {
-            beforeEach(()=> {
-                calcObj.initialValue = true;
-                calcObj.operator = true;
-                calcObj.lastValue = false;
-            });
-            it("should test when operator is true", function () {
-                calcObj.setValue("10.20");
-                expect(calcObj.lastValue).toBe('10.20');
-            });
-            it("should test when operator is true and this.lastValue.indexOf('.') === 0", function () {
-                calcObj.setValue(".");
-                expect(calcObj.lastValue).toBe('0.');
-            });
-            it("should test when lastValue is true", function () {
-                calcObj.lastValue = "20";
-                calcObj.setValue(".");
-                expect(calcObj.lastValue).toBe('20.');
-            });
-            it("should test when lastValue is true and val === '.' && this.lastValue.indexOf('.') >= 0", function () {
-                calcObj.lastValue = ".";
-                calcObj.setValue(".");
-                expect(calcObj.lastValue).toBe('0.');
-            });
-            it("shoduld test when (displayHtml.toString().length > 28)", function () {
-                calcObj.initialValue = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnop";
-                calcObj.lastValue = "";
-                calcObj.setValue(".");
-                expect(calcObj.display.innerHTML).toBe("qrstuvwxyzabcdefghijklmnop0.")
-            });
+        it("should eval the result", ()=> {
+            calcObj._eqnArr = ["7", "+"];
+            calcObj._result = "9";
+            calcObj.getResult();
+            expect(calcObj._displayEqnDiv.innerHTML).toBe('');
+            expect(calcObj._displayResultDiv.innerHTML).toBe('16');
         });
     });
-    describe("test suite to get the result according to the operator", function () {
-       var jq;
-        beforeEach(function () {
-            spyOn(window.$.fn, "attr").and.callThrough();
-            spyOn(window.$.fn, "removeAttr").and.callThrough();
-            spyOn(window, "setTimeout").and.callFake(function (param) {
-                param();
-            });
-            calcObj.initialValue = 10;
-            calcObj.lastValue = -20;
-        });
-        afterEach(function () {
-            $.fn.attr.and.callThrough();
-            $.fn.removeAttr.and.callThrough();
-            window.setTimeout.and.callThrough();
-        });
-        it("when result is not equal to blank string", function () {
-            calcObj.initialValue = '20';
-            calcObj.getResult("+");
-            expect(calcObj.initialValue).toBe('20');
-            expect(calcObj.operator).toBe("+");
-            expect(calcObj.lastValue).toBe("");
-            expect(calcObj.dispResult).toBe(true);
-            expect(calcObj.display.innerHTML).toBe(20);
-            expect($.fn.attr).toHaveBeenCalled();
-        });
-        it("when operator is +",function () {
-            calcObj.operator = "+";
-            calcObj.getResult("+");
-            expect(calcObj.initialValue).toBe(30);
-        });
-        it("when operator is -",function () {
-            calcObj.operator = "-";
-            calcObj.getResult("-");
-            expect(calcObj.initialValue).toBe(-10);
-        });
-        it("when operator is -",function () {
-            calcObj.operator = "*";
-            calcObj.getResult("*");
-            expect(calcObj.initialValue).toBe(-200);
-        });
-        it("when operator is -",function () {
-            calcObj.operator = "/";
-            calcObj.getResult("/");
-            expect(calcObj.initialValue).toBe(-0.5);
-        });
-    });
-    describe("test suite for memoryOperations", function () {
-        var dispObj = {innerHTML:"2"};
-        beforeEach(function () {
-            spyOn(document,"getElementById").and.returnValue(dispObj);
-        });
-        afterEach(function () {
-            document.getElementById.and.callThrough();
-        });
-        it("should test for memoryOperations when type ms", function () {
-            calcObj.initialValue = "20";
-            calcObj.memoryOperations("ms");
-            expect(calcObj.memoryData).toBe("20");
-            expect(dispObj.innerHTML).toBe("M");
-        });
-        it("should test for memoryOperations when type mr", function () {
-            calcObj.memoryData = "20";
-            calcObj.memoryOperations("mc");
-            expect(calcObj.memoryData).toBe("");
-            expect(dispObj.innerHTML).toBe("");
-        });
-        it("should test for memoryOperations when type mPlus", function () {
-            calcObj.display.innerHTML = NaN;
-            calcObj.memoryData = 10;
-            calcObj.memoryOperations("mPlus");
-            expect(calcObj.memoryData).toBe(10);
-        });
-        it("should test for memoryOperations when type mMinus", function () {
-            calcObj.display.innerHTML = NaN;
-            calcObj.memoryData = 10;
-            calcObj.memoryOperations("mMinus");
-            expect(calcObj.memoryData).toBe(10);
-        });
 
+    describe("negateValue method", ()=> {
+        it("should return if result is undefined", ()=> {
+            calcObj._isResultUndefined = true;
+            expect(calcObj.negateValue()).toEqual(undefined);
+        });
+        it("should negate the result", ()=> {
+            calcObj._result = "12";
+            calcObj.negateValue();
+            expect(calcObj._displayResultDiv.innerHTML).toBe('-12');
+        });
     });
+
+
 });
 
